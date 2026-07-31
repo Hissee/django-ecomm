603befad2ed029249306276fd1d73f7dc1507373
Hissee <maharjanhissee@gmail.com>
2026-07-31 09:35:23 +0545

    Product model added. List and filteration for product and category added to admin pannel

--- products/admin.py
+++ products/admin.py
@@ -3,4 +3,21 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Category)
+# For adding more properties to the listin admin pannel
+class CategoryAdmin(admin.ModelAdmin):
+    # to display in table
+    list_display = ['name', 'id', 'created_at']
+    # For search bar
+    search_fields = ['name']
+
+# need to register the Properties class
+admin.site.register(Category, CategoryAdmin)
+
+# for Products
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'selling_price', 'original_price','trending', 'stock', 'created_at']
+    search_fields = ['name', 'description', 'category__name']
+    list_filter = ['category','trending', 'created_at']
+    list_editable = ['trending', 'stock', 'selling_price']
+
+admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -1,10 +1,31 @@
 from django.db import models
 
 # Create your models here.
+# model is like database table
 class Category(models.Model):
+    # fields of the database table
     name = models.CharField(max_length=100, unique=True)
     desciption = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    original_price = models.DecimalField(max_digits=7,decimal_places=2)
+    selling_price = models.DecimalField(max_digits=7,decimal_places=2)
+    stock = models.IntegerField()
+    # on_delete = models.CASCADE mean if the category is delete the products with deleted category is also deleted
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    trending = models.BooleanField(default=False)
+    tag = models.SlugField()
+    # image = models.URLField()
+    image = models.FileField(upload_to='static/uploads')
+    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
+
+
+    def __str__(self):
+        return f'Product: {self.name}'
+    
