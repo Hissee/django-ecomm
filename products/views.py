21f675cf025a37394535828375c3a970fde23e3f
Hissee <maharjanhissee@gmail.com>
2026-08-02 09:09:57 +0545

    rendering views with products

--- products/views.py
+++ products/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
     # return HttpResponse("Hello World")
-    return render(request, 'index.html')
+    context = {
+        'products': Product.objects.all()
+    }
+    return render(request, 'index.html',context)
 def about(request):
     return render(request, 'about.html')
