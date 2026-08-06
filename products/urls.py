ba3ed8d99237f89de2edacf34a1055bec897b7dc
Hissee <maharjanhissee@gmail.com>
2026-08-06 09:39:54 +0545

    Update product

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path("allproducts",allprod,name='all-products'),
     path("addproducts",addproduct,name='add-products'),
-    path("delete-product/<int:product_id>/", deleteProduct)
+    path("delete-product/<int:product_id>/", deleteProduct),
+    path("update-product/<int:product_id>/", updateProduct, name='update-product'),
 ]
--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
 from .forms import *
+from django.contrib import messages
 
 # Create your views here.
 # def index(request):
@@ -35,3 +36,16 @@ def deleteProduct(request, product_id):
     product = Product.objects.get(id = product_id)
     product.delete()
     return redirect('all-products')
+
+def updateProduct(request, product_id):
+    product = Product.objects.get(id = product_id)
+    if request.method == 'POST':
+        productForm = ProductForm(request.POST,request.FILES, instance=product)
+        if productForm.is_valid() :
+            productForm.save()
+            return redirect('all-products')
+    context = {
+        # instace is to fill product detail in product form
+        'productForm': ProductForm(instance=product)
+    }
+    return render(request, 'products/updateproduct.html', context)
