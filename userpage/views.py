ab4b8c6b29b6846d9c6fe7f1b75a156f06a179c6
Hissee <maharjanhissee@gmail.com>
2026-08-21 09:29:19 +0545

    header footer completed

--- userpage/views.py
+++ userpage/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from products.models import *
-
 # Create your views here.
 
 def index(request):
     context = {
         # 'products' : Product.objects.all()
-        'products' : Product.objects.filter(trending = True).order_by('-id')[:4]
+        'trendingProducts' : Product.objects.filter(trending = True).order_by('-id')[:4]
+        
     }
     return render(request, 'design/index.html',context)
+
+def allprod(request):
+    context = {
+        'products': Product.objects.all()
+        
+    }
+    return render(request, 'design/products.html',context)
